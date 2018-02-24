46537782afd57311249be6de6eba38927e92ac33
nangthu3 <quanghuyfun@gmail.com>
2018-02-24 21:33:58 +0700

    Update main.js

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ function playVideo(stream, videoTag){
 // openStream()
 // .then(stream => playVideo(stream, "localStream"));
 
-const peer = new Peer({key: '7pa32uuzr64oyldi'});
+const peer = new Peer({key: 'peerjs', host: 'https://mypeerserver24022018.herokuapp.com/'});
 peer.on('open', id => {
 	$('#your_id').append(id);
 	$('#btnSignUp').click(()=>{
